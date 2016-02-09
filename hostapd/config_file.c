b8167c867c667f99b1c1216df119c84e18f4dd37
Srinivasa Duvvuri <sduvvuri@chromium.org>
2016-02-08 17:59:40 -0800

    CHROMIUMOS: hostapd: add config option to disable 40mhz scan.
    This will be turned on when mesh is turned on so that both
    mesh and ap interfaces use the same control channel.
    
    BUG=b:27067896
    TEST=tested by enabling and disabling the flag and
    	make sure from logs that hostapd is disbling
    	and enabling scan accordingly.
    
    [ ported from https://chrome-internal-review.googlesource.com/248015]
    
    Change-Id: Ib423e7576377374c01630bb2ba84f8a9e92a75de
    Reviewed-on: https://chromium-review.googlesource.com/329056
    Commit-Ready: Grant Grundler <grundler@chromium.org>
    Tested-by: Grant Grundler <grundler@chromium.org>
    Reviewed-by: Srinivasa duvvuri <sduvvuri@chromium.org>
    Reviewed-by: Grant Grundler <grundler@chromium.org>

--- hostapd/config_file.c
+++ hostapd/config_file.c
@@ -3409,6 +3409,8 @@ static int hostapd_config_fill(struct hostapd_config *conf,
 	} else if (os_strcmp(buf, "no_auth_if_seen_on") == 0) {
 		os_free(bss->no_auth_if_seen_on);
 		bss->no_auth_if_seen_on = os_strdup(pos);
+	} else if (os_strcmp(buf, "disable_40mhz_scan") == 0) {
+		conf->disable_40mhz_scan = atoi(pos);
 	} else {
 		wpa_printf(MSG_ERROR,
 			   "Line %d: unknown configuration item '%s'",
