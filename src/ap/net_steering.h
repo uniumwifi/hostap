4f9556481831e2f0ca43dc0dd6233fe854a53e53
David Weidenkopf <dweidenkopf@cococorp.com>
2016-04-21 16:42:06 -0700

    Steering: add missing header
    Signed-off-by: David Weidenkopf <dweidenkopf@cococorp.com>
    Signed-off-by: Alexis Green <agreen@cococorp.com>

---
+++ src/ap/net_steering.h
@@ -0,0 +1,13 @@
+#ifndef NETSTEERING_H
+#define NETSTEERING_H
+
+struct sta_info;
+struct hostapd_data;
+
+int net_steering_init(struct hostapd_data *hapd);
+void net_steering_deinit(struct hostapd_data *hapd);
+void net_steering_association(struct hostapd_data *hapd, struct sta_info *sta);
+void net_steering_disassociation(struct hostapd_data *hapd, struct sta_info *sta);
+
+
+#endif
